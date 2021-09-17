884f6b1ad75e2561ccad9919a0074966144bf424
zed1921 <zewge1921@gmail.com>
2021-09-17 05:23:07 -0700

    poject 0x0A-argc_argv README

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * main - prints its name + \n
- * @argc: number of arguments typed
- * @argv: array pointing to arguements
- * Return: 0
-*/
 
-int main(int argc, char *argv[])
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-argc = 0;
 printf("%s\n", argv[0]);
+
 return (0);
 }
